69119990c1d83bd4fbcc9d62e8439608c5d126db
difof <hesyoamaster@protonmail.com>
2025-03-10 11:21:01 +0330

    chore: added default eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,13 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
-})
+export default defineConfig([
+    // TypeScript files
+    {
+        extends: compat.extends(
+            "eslint:recommended",
+            "plugin:@typescript-eslint/recommended",
+            "plugin:prettier/recommended"
+        ),
+    },
+])
