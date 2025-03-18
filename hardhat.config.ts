0f4de2680a4b5feaaf573454892e48ec9f06eaf2
difof <hesyoamaster@protonmail.com>
2025-03-18 11:17:04 +0330

    chore: import new hardhat stuff

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,2 +1,5 @@
 import dotenv from "dotenv"
 dotenv.config({ quiet: true })
+
+import type { HardhatUserConfig } from "hardhat/config"
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers"
