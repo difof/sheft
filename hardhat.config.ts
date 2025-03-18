a9c62b1ce7fe0a6e520fabca36c9f992f42ccb41
difof <hesyoamaster@protonmail.com>
2025-03-18 11:19:48 +0330

    chore: export hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,3 +3,8 @@ dotenv.config({ quiet: true })
 
 import type { HardhatUserConfig } from "hardhat/config"
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers"
+
+const config: HardhatUserConfig = {
+}
+
+export default config
