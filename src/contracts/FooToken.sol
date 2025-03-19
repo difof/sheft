c4ff2d730cbf1221199cdf1bc0aabf5cbc5bf47d
difof <hesyoamaster@protonmail.com>
2025-03-19 12:03:54 +0330

    feat: added ctor for token

--- src/contracts/FooToken.sol
+++ src/contracts/FooToken.sol
@@ -5,4 +5,7 @@ pragma solidity 0.8.23;
 import { ERC20 } from "openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FooToken is ERC20 {
+    constructor(uint256 _totalSupply, address _mintFor) ERC20("NAME", "SYM") {
+        _mint(_mintFor, _totalSupply);
+    }
 }
