5dcd5f6052480938b3eb9604a711dcaab12dad5c
difof <hesyoamaster@protonmail.com>
2025-03-20 11:25:54 +0330

    chore: added Airdrop small stuff

--- src/contracts/Airdrop.sol
+++ src/contracts/Airdrop.sol
@@ -1,3 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.23;
+contract Airdrop is
+    bytes32 public merkleRoot = 0x0;
+    event MerkleRootUpdated(bytes32 indexed previous, bytes32 indexed updated);
+}
