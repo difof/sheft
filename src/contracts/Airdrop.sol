eb49719f02c20722c3c074a86017448f2178e5f5
difof <hesyoamaster@protonmail.com>
2025-03-20 11:29:02 +0330

    feat: the Airdrop is ownable

--- src/contracts/Airdrop.sol
+++ src/contracts/Airdrop.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.23;
+import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
 contract Airdrop is
+    Ownable,
+{
     bytes32 public merkleRoot = 0x0;
     event MerkleRootUpdated(bytes32 indexed previous, bytes32 indexed updated);
+    constructor(
+        address _admin
+    ) Ownable(_admin) { }
 }
