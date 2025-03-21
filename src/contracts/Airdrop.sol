4afae42d9c7bbe9b82b36a30a8ad46a0c4cc9e48
difof <hesyoamaster@protonmail.com>
2025-03-21 11:23:24 +0330

    feat: added internal token transfer function

--- src/contracts/Airdrop.sol
+++ src/contracts/Airdrop.sol
@@ -2,9 +2,15 @@
 
 pragma solidity 0.8.23;
 import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from
+    "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Address } from "openzeppelin/contracts/utils/Address.sol";
 contract Airdrop is
     Ownable,
 {
+    using Address for address payable;
+    using SafeERC20 for IERC20;
     bytes32 public merkleRoot = 0x0;
     event MerkleRootUpdated(bytes32 indexed previous, bytes32 indexed updated);
     event ReceivedNative(address indexed sender, uint256 amount);
@@ -13,5 +19,16 @@ contract Airdrop is
     ) Ownable(_admin) { }
     receive() external payable {
         emit ReceivedNative(msg.sender, msg.value);
+    }
+    function _transferToken(
+        IERC20 _token,
+        address _to,
+        uint256 _amount
+    ) internal {
+        if (address(_token) == address(0)) {
+            payable(_to).sendValue(_amount);
+        } else {
+            _token.safeTransfer(_to, _amount);
+        }
     }
 }
