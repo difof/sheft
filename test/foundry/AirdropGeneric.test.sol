3b6f04c99f5d03bf62eb52f9e4f89782e7888a46
difof <hesyoamaster@protonmail.com>
2025-03-26 12:25:49 +0330

    test: added airdrop generic test sc

--- test/foundry/AirdropGeneric.test.sol
+++ test/foundry/AirdropGeneric.test.sol
@@ -1,3 +1,13 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.23;
+
+import { MerkleTreeLib } from "solady/utils/MerkleTreeLib.sol";
+import { IERC20 } from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { AirdropSetup } from "./Airdrop.setup.sol";
+
+import "../../src/contracts/Airdrop.sol";
+
+contract Test_Airdrop_Generic is AirdropSetup {
+}
