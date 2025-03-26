fb4bd7eab7d9da006027ff2ed3df8b818b8a5df9
difof <hesyoamaster@protonmail.com>
2025-03-26 11:11:08 +0330

    test: added imports

--- test/foundry/Airdrop.setup.sol
+++ test/foundry/Airdrop.setup.sol
@@ -3,3 +3,9 @@
 pragma solidity 0.8.23;
 
 import { Test } from "forge-std/Test.sol";
+
+import { IERC20 } from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { MerkleTreeLib } from "solady/utils/MerkleTreeLib.sol";
+
+import "../../src/contracts/FooToken.sol";
+import "../../src/contracts/Airdrop.sol";
