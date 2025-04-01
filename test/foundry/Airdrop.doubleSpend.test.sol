68c698b31a620eb03d060bcca39bb3d9af3e6474
difof <hesyoamaster@protonmail.com>
2025-04-01 11:39:29 +0330

    test: added double spend test file

---
+++ test/foundry/Airdrop.doubleSpend.test.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import { MerkleTreeLib } from "solady/utils/MerkleTreeLib.sol";
+import { IERC20 } from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { AirdropSetup } from "./Airdrop.setup.sol";
+
+import "../../src/contracts/Airdrop.sol";
+
