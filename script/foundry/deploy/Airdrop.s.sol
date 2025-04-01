6ba16df0a57c5438cd5a228ac6783e41f66123f7
difof <hesyoamaster@protonmail.com>
2025-04-01 12:15:23 +0330

    feat: airdrop deploy script

--- script/foundry/deploy/Airdrop.s.sol
+++ script/foundry/deploy/Airdrop.s.sol
@@ -5,3 +5,19 @@ pragma solidity 0.8.23;
 import { Script } from "forge-std/Script.sol";
 
 import "../../../src/contracts/Airdrop.sol";
+
+contract Deploy_Airdrop is Script {
+    function run() public {
+        address owner = vm.getWallets()[0];
+
+        vm.startBroadcast();
+        deploy(owner);
+        vm.stopBroadcast();
+    }
+
+    function deploy(
+        address _owner
+    ) public returns (address airdrop) {
+        return address(new Airdrop(_owner));
+    }
+}
