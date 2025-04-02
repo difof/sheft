1479853c3c78d46a55016a6f28b18a2ea6001d2d
difof <hesyoamaster@protonmail.com>
2025-04-02 12:13:09 +0330

    feat: keccak script export

--- src/ts/keccak.ts
+++ src/ts/keccak.ts
@@ -3,3 +3,10 @@ import createHash from "keccak"
 function withHash(): createHash.Keccak {
     return createHash("keccak256")
 }
+
+export default function (input: string): string {
+    if (input.startsWith("0x")) {
+        return withHash().update(input.slice(2), "hex").digest("hex")
+    }
+    return withHash().update(input).digest("hex")
+}
