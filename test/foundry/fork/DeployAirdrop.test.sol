3c75ec441f5feec4d88d7d415868d1d4b936500f
difof <hesyoamaster@protonmail.com>
2025-04-02 11:31:11 +0330

    test: setup airdrop fork test

--- test/foundry/fork/DeployAirdrop.test.sol
+++ test/foundry/fork/DeployAirdrop.test.sol
@@ -3,3 +3,17 @@
 pragma solidity 0.8.23;
 
 import { Test } from "forge-std/Test.sol";
+
+import { Deploy_Airdrop } from "../../../script/foundry/deploy/Airdrop.s.sol";
+import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
+
+contract TestFork_DeployAirdrop is Test {
+    address owner = vm.addr(0x200);
+
+    Deploy_Airdrop deployScript;
+
+    function setUp() public {
+        deployScript = new Deploy_Airdrop();
+    }
+
+}
