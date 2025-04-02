593c0689e2e0b30bd0e09d24dee6608cd7e6b636
difof <hesyoamaster@protonmail.com>
2025-04-02 11:19:35 +0330

    feat: implemented foo token deploy script

--- script/foundry/deploy/FooToken.sol
+++ script/foundry/deploy/FooToken.sol
@@ -6,3 +6,20 @@ import { Script } from "forge-std/Script.sol";
 
 import "../../../src/contracts/FooToken.sol";
 
+contract Deploy_FooToken is Script {
+    function run() public {
+        address owner = vm.getWallets()[0];
+        uint256 totalSupply = 777 ether;
+
+        vm.startBroadcast();
+        deploy(totalSupply, owner);
+        vm.stopBroadcast();
+    }
+
+    function deploy(
+        uint256 _totalSupply,
+        address _owner
+    ) public returns (address token) {
+        return address(new FooToken(_totalSupply, _owner));
+    }
+}
