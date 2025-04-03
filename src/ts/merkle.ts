542debc3aefea08a2b7f660d1268d840e3f357a9
difof <hesyoamaster@protonmail.com>
2025-04-03 11:18:59 +0330

    feat: merkle utility fields and types

--- src/ts/merkle.ts
+++ src/ts/merkle.ts
@@ -1,7 +1,17 @@
 import { MerkleTree } from "merkletreejs"
 
 export type DataItem = object
+export type HexString = string
+
+export type MapperFunction<T extends DataItem> = (item: T) => string
+export type HashFunction = (input: string) => HexString
+
 class MerkleWrapper<T extends DataItem> {
+    readonly #mapperFunction: MapperFunction<T>
+    readonly #hashFunction: HashFunction
+    readonly #tree: MerkleTree
+    readonly #hashedData: HexString[]
+
 }
 
 export default MerkleWrapper
