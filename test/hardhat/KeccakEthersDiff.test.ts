a1b73dff6ddda57f10b60a0a382496b38513c1e5
difof <hesyoamaster@protonmail.com>
2025-04-15 12:14:42 +0330

    test: keccak and ethers diff test

--- test/hardhat/KeccakEthersDiff.test.ts
+++ test/hardhat/KeccakEthersDiff.test.ts
@@ -1,3 +1,15 @@
 import { network } from "hardhat"
 import { expect } from "chai"
 import keccak from "../../src/ts/keccak.ts"
+
+const { ethers } = await network.connect()
+
+describe("Diff keccak lib and ethers", () => {
+    it("Should result in same hash from keccak and ethers.keccak256", () => {
+        const valueToHash = "Hello, World!"
+        const encoded = ethers.solidityPacked(["string"], [valueToHash])
+        const ethersKeccakHash = ethers.keccak256(encoded)
+        const keccakHash = "0x" + keccak(encoded)
+        expect(keccakHash).to.eq(ethersKeccakHash)
+    })
+})
