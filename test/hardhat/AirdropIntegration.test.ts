028851c94643460741588d82ebbeac6d474b2f35
difof <hesyoamaster@protonmail.com>
2025-04-16 11:08:29 +0330

    chore: add typechain imports

--- test/hardhat/AirdropIntegration.test.ts
+++ test/hardhat/AirdropIntegration.test.ts
@@ -2,3 +2,12 @@ import { expect } from "chai"
 import { describe, it, beforeEach } from "mocha"
 import { network } from "hardhat"
 import Merkle from "../../src/ts/merkle.ts"
+import {
+    type FooToken,
+    FooToken__factory,
+    type Airdrop,
+    Airdrop__factory,
+} from "../../typechain/index.ts"
+import { type AirdropMembershipStruct } from "../../typechain/Airdrop.sol/Airdrop.ts"
+
+const { ethers } = await network.connect()
