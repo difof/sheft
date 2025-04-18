3e83e75294dae0ab2c23e3399afd23ca0d054807
difof <hesyoamaster@protonmail.com>
2025-04-18 11:39:29 +0330

    feat: re-export of merkle

--- package/index.ts
+++ package/index.ts
@@ -2,3 +2,18 @@ export * from "../typechain"
 
 import keccak from "../src/ts/keccak"
 export { keccak }
+
+import Merkle from "../src/ts/merkle"
+import type {
+    DataItem,
+    HashFunction,
+    HexString,
+    MapperFunction,
+} from "../src/ts/merkle"
+export {
+    Merkle,
+    DataItem as MerkleDataItem,
+    HashFunction as MerkleHashFunction,
+    HexString as MerkleHexString,
+    MapperFunction as MerkleMapperFunction,
+}
