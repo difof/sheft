981d4dc622af908a7de238510078d84e77cc1d83
difof <hesyoamaster@protonmail.com>
2025-05-16 11:21:53 +0330

    feat: some colorful logging

--- hooks/post_scaffold.sh
+++ hooks/post_scaffold.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+BOLD="\033[1m"
+RESET="\033[0m"
+GREEN="\033[32m"
+RED="\033[31m"
+YELLOW="\033[33m"
+BLUE="\033[36m"
+MAGENTA="\033[35m"
+
+info()  { echo -e "${BLUE}${BOLD}ℹ${RESET}${BOLD} $* ${RESET}"; }
+ok()    { echo -e "${GREEN}${BOLD}✔${RESET}${BOLD} $* ${RESET}"; }
+info_input()    { echo -e "${RESET}${BOLD}${YELLOW} $* ${RESET}"; }
+fail()  { echo -e "${RED}${BOLD}❌${RESET}${BOLD} $* ${RESET}"; exit 1; }
+
