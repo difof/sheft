52af93f56413c3c01eb68a445a8a18ba69211810
difof <hesyoamaster@protonmail.com>
2025-09-28 11:22:13 +0330

    refactor: renamed FooToken with project name template

--- {{ .Project }}/src/contracts/{{.ProjectPascal}}Token.sol
+++ {{ .Project }}/src/contracts/{{.ProjectPascal}}Token.sol
@@ -1,20 +1,20 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: {{.Scaffold.license}}
 
-pragma solidity 0.8.23;
+pragma solidity {{.Scaffold.solc_version}};
 
 import { ERC20 } from "openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @title FooToken
+/// @title {{.ProjectPascal}}Token
 /// @notice A basic ERC20 token with fixed supply minted at deployment.
 /// @dev This token has no pausable functionality and mints the entire supply to a specified address during construction.
-contract FooToken is ERC20 {
-    /// @notice Creates the FooToken with a fixed total supply.
+contract {{.ProjectPascal}}Token is ERC20 {
+    /// @notice Creates the {{.ProjectPascal}}Token with a fixed total supply.
     /// @dev Mints the entire token supply to the specified address during deployment.
     ///      There is no additional minting functionality after deployment.
     ///      This contract has no pausable features - transfers cannot be halted.
     /// @param _totalSupply The total number of tokens to mint (18 decimals).
     /// @param _mintFor The address that will receive the entire token supply.
-    constructor(uint256 _totalSupply, address _mintFor) ERC20("NAME", "SYM") {
+    constructor(uint256 _totalSupply, address _mintFor) ERC20("ERC20Token", "ETK") {
         _mint(_mintFor, _totalSupply);
     }
 }
